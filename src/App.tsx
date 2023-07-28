f2393bb5d0d45b1310513e79ef056329e88fa91c
Kostas Thomson <kostasthomson12@gmail.com>
2023-07-29 02:12:18 +0300

    Fetch from server
    Added Axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
+// import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const url = "http://localhost:8080/people";
+  const [data, setData] = useState([]);
+
+  const fetchData = async () => {
+    const res = await axios.get(url);
+    console.log(res.data._embedded.people);
+    return setData(res.data._embedded.people);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
