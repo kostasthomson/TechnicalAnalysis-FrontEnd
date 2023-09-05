910e41a6b7fa8f85c9bfbc9191a6fec13afe8677
Kostas Thomson <kostasthomson12@gmail.com>
2023-09-06 00:29:18 +0300

    Optimization
    - API calls optimization
    - Mapping optimization

--- src/App.tsx
+++ src/App.tsx
@@ -1,72 +1,48 @@
-import React, { useState, useEffect } from "react";
-// import logo from './logo.svg';
-import axios, { AxiosResponse } from "axios";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 import "./App.css";
-
-import { CollaboratorType, CommitsType } from "./Types";
-
-import CommitList from "./Components/Commit";
-import CollaboratorList from "./Components/Collaborator";
 
 function App() {
   const base_url = "http://localhost:8080/";
 
-  const [collaborator, setCollaborator] = useState<CollaboratorType>();
-
-  const [commits, setCommits] = useState<CommitsType[]>([]);
-
-  const collaborators: CollaboratorType[] = [];
-
-  // const [commitsMap, setCommitsMap] = useState<Map<string, CommitsType>>();
-
-  const fetchData = async () => {
-    // await axios
-    //   .get(base_url + "collaborators")
-    //   .then((res: AxiosResponse<any, any>) =>
-    //     setCollaborators(res.data._embedded.collaborators)
-    //   )
-    //   .catch(() => console.log("Error collaborators"));
-    await axios
-      .get(base_url + "commits")
-      .then((res: AxiosResponse<any, any>) =>
-        setCommits(res.data._embedded.commits)
-      )
-      .catch(() => console.log("Error commits"));
-    return;
-  };
-
-  const fetchCollaborator = async (link: string) => {
-    await axios
-      .get(link)
-      .then((res: AxiosResponse<any, any>) =>
-        collaborators.push(res.data)
-      )
-      .catch(() => console.log("Error collaborators"));
-  }
+  const [collaboratorList, setCollaboratorList] = useState([]);
+  const [commitList, setCommitList] = useState([]);
 
   useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const [collaboratorsResponse, commitsResponse] = await Promise.all([
+          axios.get(base_url + "collaborators"),
+          axios.get(base_url + "commits"),
+        ]);
+
+        setCollaboratorList(collaboratorsResponse.data._embedded.collaborators);
+        setCommitList(commitsResponse.data._embedded.commits);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
     fetchData();
   }, []);
 
-  const map = new Map<string, CommitsType>();
-  commits.forEach((commit) => map.set(commit.sha, commit));
+  useEffect(() => {
+    if (collaboratorList.length > 0 && commitList.length > 0) {
+      const mapper: Map<String, never[]> = new Map();
+      collaboratorList.forEach((collaborator: any) => {
+        mapper.set(
+          collaborator.stringId,
+          commitList.filter((commit: any) => {
+            return commit.authorId === collaborator.stringId;
+          })
+        );
+      });
+      console.log(mapper);
+    }
+  }, [collaboratorList, commitList]);
 
-  const linksMap = new Map<string, string>();
-  map.forEach((value, key) => linksMap.set(key, value._links.author.href));
-
-  linksMap.forEach((link) => fetchCollaborator(link));
-
-  console.log(collaborators);
-
-  return (
-    <div className="App">
-      {/* <CollaboratorList data={collaborators} />
-      <CommitList
-        data={commits.sort((a, b) => Date.parse(a.date) - Date.parse(b.date))}
-      /> */}
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
